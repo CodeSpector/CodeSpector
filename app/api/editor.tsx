fa379c13246ccc1d3f8ecb79637a08b212a4a96b
Varad Sharma <varad.code19@gmail.com>
2023-10-08 22:44:25 +0530

    api test

---
+++ app/api/editor.tsx
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(res: NextApiResponse, req: NextApiRequest) {
+    res.status(200).json({ editor: 'VSCode+Neovim' });
+}
