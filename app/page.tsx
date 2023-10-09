daa681318263ca3e62cee869604dc839345ccdcd
Varad Sharma <varad.code19@gmail.com>
2023-10-09 19:05:03 +0530

    more stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,76 @@
 import NextLink from "next/link";
+import NextImage from "next/image";
 import { Link } from "@nextui-org/link";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { title, subtitle } from "@/components/primitives";
+import Image from "next/image";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-left justify-left gap-4 py-8 md:py-10 bg-herobg before:blur-2xl before:bg-gradient-readial after:blur-2xl after:bg-gradient-conic">
-			<div className={"bg-gradient-to-r from-sky-500 to-indigo-500 h-full z-1"}>
+    return (
+        <section className="flex flex-col items-left justify-left gap-4 py-8 md:py-10 bg-herobg before:blur-2xl before:bg-gradient-readial after:blur-2xl after:bg-gradient-conic">
+            <div
+                className={
+                    "bg-gradient-to-r from-sky-500 to-indigo-500 h-full z-1"
+                }
+            ></div>
+            <div className="inline-block text-left justify-left z-2">
+                <h1 className={title()}>Code</h1>
+                <h1 className={title({ color: "violet" })}>Spector</h1>
+                <br />
+                <br />
+                <h1 className={subtitle({ class: "mt-3 font-bold" })}>
+                    Helping Bussinesses Established On The Internet
+                </h1>
+                <p className={subtitle({})}>
+                    In today&spos;s digital world, it is more important than
+                    ever for businesses to have a strong online presence. We are
+                    a team of experienced web developers and designers who can
+                    help you create a website that is both visually appealing
+                    and easy to use. We will also help you optimize your website
+                    for search engines so that potential customers can easily
+                    find you online.
+                </p>
+            </div>
 
-			</div>
-			<div className="inline-block text-left justify-left z-2">
-				<h1 className={title()}>Code</h1>
-				<h1 className={title({ color: "violet" })}>Spector</h1>
-				<br /><br />
-				<h1 className={subtitle({ class: "mt-3 font-bold" })}>
-					Helping Bussinesses Established On The Internet
-				</h1>
-				<p className={subtitle({})}>
-					In today&spos;s digital world, it is more important than ever for businesses to have a strong online presence. We are a team of experienced web developers and designers who can help you create a website that is both visually appealing and easy to use. We will also help you optimize your website for search engines so that potential customers can easily find you online.
-				</p>
-			</div>
-
-			<div className="flex gap-3">
-				<Link
-					as={NextLink}
-					href="/services"
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Get Started Now
-				</Link>
-				<Link
-					as={NextLink}
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href="/contact"
-				>
-					Contact Us
-				</Link>
-			</div>
-
-		</section>
-	);
+            <div className="flex gap-3">
+                <Link
+                    as={NextLink}
+                    href="/services"
+                    className={buttonStyles({
+                        color: "primary",
+                        radius: "full",
+                        variant: "shadow",
+                    })}
+                >
+                    Get Started Now
+                </Link>
+                <Link
+                    as={NextLink}
+                    className={buttonStyles({
+                        variant: "bordered",
+                        radius: "full",
+                    })}
+                    href="/contact"
+                >
+                    Contact Us
+                </Link>
+            </div>
+            <div className="flex flex-col items-right justify-right ">
+				<div>
+				</div>
+                <div className="flex flex-col text-right">
+                    <h1 className={title({ class: "mt-3 font-bold" })}>
+                        What We Do
+                    </h1>
+                    <p className={subtitle({})}>
+                        We are a team of experienced web developers and
+                        designers who can help you create a website that is both
+                        visually appealing and easy to use. We will also help
+                        you optimize your website for search engines so that
+                        potential customers can easily find you online.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 }
