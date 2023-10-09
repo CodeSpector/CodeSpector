90d0092f006a59d5c78401ba81e386ff0fb7586a
Varad Sharma <varad.code19@gmail.com>
2023-10-09 21:30:38 +0530

    more

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,97 @@
+/* eslint-disable @next/next/no-img-element */
 import NextLink from "next/link";
-import NextImage from "next/image";
 import { Link } from "@nextui-org/link";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { title, subtitle } from "@/components/primitives";
-import Image from "next/image";
 
 export default function Home() {
     return (
-        <section className="flex flex-col items-left justify-left gap-4 py-8 md:py-10 bg-herobg before:blur-2xl before:bg-gradient-readial after:blur-2xl after:bg-gradient-conic">
-            <div
-                className={
-                    "bg-gradient-to-r from-sky-500 to-indigo-500 h-full z-1"
-                }
-            ></div>
-            <div className="inline-block text-left justify-left z-2">
-                <h1 className={title()}>Code</h1>
-                <h1 className={title({ color: "violet" })}>Spector</h1>
-                <br />
-                <br />
-                <h1 className={subtitle({ class: "mt-3 font-bold" })}>
-                    Helping Bussinesses Established On The Internet
-                </h1>
-                <p className={subtitle({})}>
-                    In today&spos;s digital world, it is more important than
-                    ever for businesses to have a strong online presence. We are
-                    a team of experienced web developers and designers who can
-                    help you create a website that is both visually appealing
-                    and easy to use. We will also help you optimize your website
-                    for search engines so that potential customers can easily
-                    find you online.
-                </p>
-            </div>
-
-            <div className="flex gap-3">
-                <Link
-                    as={NextLink}
-                    href="/services"
-                    className={buttonStyles({
-                        color: "primary",
-                        radius: "full",
-                        variant: "shadow",
-                    })}
-                >
-                    Get Started Now
-                </Link>
-                <Link
-                    as={NextLink}
-                    className={buttonStyles({
-                        variant: "bordered",
-                        radius: "full",
-                    })}
-                    href="/contact"
-                >
-                    Contact Us
-                </Link>
-            </div>
-            <div className="flex flex-col items-right justify-right ">
-				<div>
-				</div>
-                <div className="flex flex-col text-right">
-                    <h1 className={title({ class: "mt-3 font-bold" })}>
-                        What We Do
+        <section className="flex flex-col items-left justify-left gap-4 py-8 md:py-10 ">
+            <div className="h-screen translate-y-[16%] flex flex-row items-start inset-0">
+                <div className="flex flex-col justify-center items-center mt-5 translate-y-0 mr-1">
+                    <div className="w-5 h-5 rounded-full bg-[#FF1CF7] inline-block "></div>
+                    <div className="w-1 sm:h-40 h-20 bg-[#FF1CF7] inline-block"></div>
+                </div>
+                <div className="inline-block text-left justify-left z-2 relative">
+                    <h1 className={title()}>Code</h1>
+                    <h1 className={title({ color: "violet" })}>Spector</h1>
+                    <br />
+                    <br />
+                    <h1 className={subtitle({ class: "mt-3 font-bold" })}>
+                        Helping Bussinesses Established On The Internet
                     </h1>
+                    <p className={subtitle({})}>
+                        In this digital world, it is more important than ever
+                        for businesses to have a strong online presence. We are
+                        a team of experienced web developers and designers who
+                        can help you create a website that is both visually
+                        appealing and easy to use. We will also help you
+                        optimize your website for search engines so that
+                        potential customers can easily find you online.
+                    </p>
+                    <div className="flex gap-3 relative">
+                        <Link
+                            as={NextLink}
+                            href="/services"
+                            className={buttonStyles({
+                                color: "primary",
+                                radius: "full",
+                                variant: "shadow",
+                            })}
+                        >
+                            Get Started Now
+                        </Link>
+                        <Link
+                            as={NextLink}
+                            className={buttonStyles({
+                                variant: "bordered",
+                                radius: "full",
+                            })}
+                            href="/#more"
+                        >
+                            Learn More
+                        </Link>
+                    </div>{" "}
+                </div>
+            </div>
+            {/* <div className="gradient"></div> */}
+            <br id="more" />
+            <div className="flex flex-row items-right justify-right ">
+                <div>
+                    <img
+                        src="/web-design.png"
+                        alt="web design"
+                        className="width-[0.1rem] height-[0.1rem] invert"
+                    />
+                </div>
+                <div className="flex flex-col text-right">
+                    <span>
+                        <h1
+                            className={title({
+                                size: "sm",
+                                class: "font-bold",
+                            })}
+                        >
+                            What
+                        </h1>
+                        <h1
+                            className={title({
+                                size: "sm",
+                                color: "violet",
+                                class: "font-bold",
+                            })}
+                        >
+                            We
+                        </h1>
+                        <h1
+                            className={title({
+                                size: "sm",
+                                class: "font-bold",
+                            })}
+                        >
+                            Do
+                        </h1>
+                    </span>
                     <p className={subtitle({})}>
                         We are a team of experienced web developers and
                         designers who can help you create a website that is both
