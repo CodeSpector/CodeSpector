cde014ba0b24d249369327bd5ff590fa77e07d7a
Varad Sharma <varad.code19@gmail.com>
2023-11-24 20:31:19 +0530

    color change

--- components/About/Head.tsx
+++ components/About/Head.tsx
@@ -7,7 +7,7 @@ import { textVariant, fadeIn } from "@/utils/motion";
 const AboutHead = () => {
 	return (
 		<motion.div variants={textVariant(2)}>
-			<h1 className={title()}><span className={title({ color:"violet" })}>About</span>Us</h1>
+			<h1 className={title()}><span className={title({ color:"blue" })}>About</span>Us</h1>
 			<p className={subtitle({})}>We are a small team with guys full of ambition. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos perspiciatis at commodi hic non illum animi consequatur assumenda atque corporis sed aspernatur aliquam adipisci, nobis, ex, inventore ullam? Error nisi inventore officiis.</p>
 		</motion.div>
 	);
--- components/Experience/Head.tsx
+++ components/Experience/Head.tsx
@@ -9,7 +9,7 @@ const ExperienceHead = () => {
         <motion.div variants={textVariant()}>
             <h1 className={title({ class: "sm:text-sm" })}>
                 Our
-                <span className={title({ color: "violet" })}>Experience</span>
+                <span className={title({ color: "blue" })}>Experience</span>
                 InTheWeb
             </h1>
             <p className={subtitle()}>We are a startup based in India, making appearence in the industry recently. We have already worked with quite multiple startups, local bussinesses and freelancers. Many of our clients have seen growth using the power of the Web. The power of the internet once harnessed and used correctly can boost ones growth.</p>
