a6de617398940cfb2a55e2247a26a2b507643c82
Varad Sharma <varad.code19@gmail.com>
2024-01-08 20:36:40 +0530

    Form update

--- components/Contact/Head.tsx
+++ components/Contact/Head.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { Button, Input } from "@nextui-org/react";
 
 const ContactHead = () => {
   return (
@@ -18,11 +19,14 @@ const ContactHead = () => {
       <div className="cnt p-5 flex flex-col md:flex-row m-5 justify-center content-center md:mr-[30%] md:ml-[30%]">
         <form action="https://formsubmit.co/codespector.buildedge@gmail.com" method="POST" className="flex flex-col">
           {" "}
-          <label htmlFor="name">Name & Message</label>{" "}
-          <input type="text" name="name" required={true} className="my-5 mt-0 rounded-md" />{" "}
-          <label htmlFor="email">email</label>{" "}
-          <input type="email" name="email" required={true} className="my-5 mt-0 rounded-md" />{" "}
-          <button type="submit">Send</button>{" "}
+          {/* <label htmlFor="name">Name & Message</label>{" "} */}
+          {/* <input type="text" name="name" required={true} className="my-5 mt-0 rounded-md" />{" "} */}
+          <Input type="email" label="Email" labelPlacement="outside" isRequired />
+          <Input type="text" label="Name" labelPlacement="outside" isRequired />
+          {/* <label htmlFor="email">email</label>{" "} */}
+          {/* <input type="email" name="email" required={true} className="my-5 mt-0 rounded-md" />{" "} */}
+          {/* <button type="submit">Send</button>{" "} */}
+          <Button type="submit" color="success" className="mt-5">Send</Button>
         </form>
         <div className="mx-5">
           <h1>Or</h1>
