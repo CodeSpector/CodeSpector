f84e0f42a80de9a31a870cb46c06ed7d4919d889
Varad Sharma <varad.code19@gmail.com>
2024-01-19 00:51:24 +0530

    hero cahnges

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -10,15 +12,13 @@ const Hero = () => {
       transition={{ duration: 1.5 }}
       className="flex flex-col justify-center content-center items-center align-middle -translate-y-10"
     >
-      <div className="bg-hero w-[99vw]">
-        <div className="h-[85vh]   p-2 translate-y-[-3rem] flex flex-col justify-center text-center md:ml-[20%] mr-[20%]">
+      <div className="bg-hero h-screen w-[99vw]">
+        <div className="h-full w-full  p-2 translate-y-[-3rem] flex flex-col justify-center align-center items-center text-center">
           <h1 className="text-6xl md:text-7xl">CodeSpector</h1>
           <p className="text-2xl">
             Helping Bussinesses & Individuals Established On The Internet
           </p>
-          <span className="text-md text-slate-500 md:translate-y-[150%]">
-            Scroll To Learn More
-          </span>
+          <Button as={Link} color="primary" href="/services" className="text-white mt-5 max-w-sm z-[1000]">Get Started</Button>
         </div>
       </div>
     </motion.div>
