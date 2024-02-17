0152717b220859091654c5a286e0e572de03b7ef
Varad Sharma <varad.code19@gmail.com>
2024-02-17 16:07:48 +0530

    hover fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({
       <body className={roboto.className}>
         <div className="">
           <Providers>
-            <Nav />
             <HoverCircle />
+            <Nav />
             {children}
             <footer>
               <Footer />
