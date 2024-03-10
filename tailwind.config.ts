4fa2b0b5f12f4425e6ba4f78ae51576c411cab62
Varad Sharma <varad.code19@gmail.com>
2024-03-10 13:16:39 +0530

    setting up for getstarted page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,11 @@ const config: Config = {
       animation: {
         "meteor-effect": "meteor 5s linear infinite",
         "sc-animation": "sc 1s ease-in-out infinite",
+	first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
       },
       keyframes: {
         meteor: {
@@ -39,6 +44,39 @@ const config: Config = {
           "0%": { transform: "translateY(0)" },
           "50%": { transform: "translateY(-10px)" },
           "100%": { transform: "translateY(0)" },
+        },
+	        moveHorizontal: {
+          "0%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+          "50%": {
+            transform: "translateX(50%) translateY(10%)",
+          },
+          "100%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+        },
+        moveInCircle: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "50%": {
+            transform: "rotate(180deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        moveVertical: {
+          "0%": {
+            transform: "translateY(-50%)",
+          },
+          "50%": {
+            transform: "translateY(50%)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
         },
       },
     },
