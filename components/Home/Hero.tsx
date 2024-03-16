ce48193dadd3fa99d7168330698f36afd852e0eb
Varad Sharma <117466176+Varad-Codes@users.noreply.github.com>
2024-03-16 07:44:51 +0000

    hot fix

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -8,39 +8,39 @@ import { Meteors } from "../ui/meteors";
 
 const Hero = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1.5 }}
-      className="flex flex-col justify-center content-center items-center align-middle -translate-y-10"
-    >
-      <div className="bg-hero-wave-v md:bg-hero-wave-h bg-no-repeat bg-cover h-screen w-[99vw]">
-        <div className="flex  flex-row"><Meteors /><Meteors className="sm:hidden/></div>
-        <div className="h-full w-full p-2 translate-y-[-3rem] flex flex-col justify-center align-center items-center">
-          <div className="text-center m-5 flex flex-col md:flex-row">
-            {/* <h1 className="text-4xl md:text-7xl">CodeSpector</h1> */}
-            <div className=" p-5 m-5 rounded-xl flex flex-col">
-              <span className="text-4xl md:text-8xl">Level up your <span className="font-bold bg-gradient-to-r from-violet-600 to-indigo-600 bg-clip-text text-transparent">online presence</span></span>
-              <p className="text-2xl md:text-5xl mt-5">With <span className="font-bold bg-gradient-to-r from-blue-600 to-violet-600 bg-clip-text text-transparent">Fast and Beautiful{" "}</span> websites <span>Tailoured{" "}</span>to <span className="font-bold">you</span></p>
+    <> {/* Open JSX fragment */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-col justify-center content-center items-center align-middle -translate-y-10"
+      >
+        <div className="bg-hero-wave-v md:bg-hero-wave-h bg-no-repeat bg-cover h-screen w-[99vw]">
+          <div className="flex  flex-row"><Meteors /><Meteors className="sm:hidden"/></div>
+          <div className="h-full w-full p-2 -translate-y-12 flex flex-col justify-center align-center items-center">
+            <div className="text-center m-5 flex flex-col md:flex-row">
+              {/* <h1 className="text-4xl md:text-7xl">CodeSpector</h1> */}
+              <div className=" p-5 m-5 rounded-xl flex flex-col">
+                <span className="text-4xl md:text-8xl">Level up your <span className="font-bold bg-gradient-to-r from-violet-600 to-indigo-600 bg-clip-text text-transparent">online presence</span></span>
+                <p className="text-2xl md:text-5xl mt-5">With <span className="font-bold bg-gradient-to-r from-blue-600 to-violet-600 bg-clip-text text-transparent">Fast and Beautiful{" "}</span> websites <span>Tailored{" "}</span>to <span className="font-bold">you</span></p>
+              </div>
             </div>
+            <div>
+              <Button
+                as={Link}
+                href="/getstarted"
+                className="m-5 text-white"
+                color="primary"
+                variant="bordered"
+              >
+                Get Started
+              </Button>
+            </div>
+            <span className="animate-sc-animation flex justify-end align-bottom">{"V"}</span>
           </div>
-          <div>
-            <Button
-              as={Link} 
-              href="/getstarted"
-              className="m-5 text-white"
-              color="primary"
-              variant="bordered"
-            >
-              Get Started
-            </Button>
-          </div>
-          <span className="animate-sc-animation flex justify-end align-bottom">{"V"}</span>
         </div>
-      </div>
-      {/* TODO: Fix the bleed not bleeding */}
-      {/* <div className="w-screen h-2 shadow-lg shadow-light-purple"></div> */}
-    </motion.div>
+      </motion.div>
+    </>
   );
 };
 
